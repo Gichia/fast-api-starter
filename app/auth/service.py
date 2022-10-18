f44047602187dd2b7b13c868f0465312a58b1efa
Gichia <petergichia35@gmail.com>
2022-10-18 07:55:43 +0300

    create method to create access token and log user in

--- app/auth/service.py
+++ app/auth/service.py
@@ -14,15 +14,21 @@ Misc variables:
 
     None
 """
-from typing import Dict
+from datetime import datetime, timedelta
+
+from jose import JWTError, jwt
 from sqlalchemy.orm import Session
 from passlib.context import CryptContext
-from fastapi import HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status
 
-from app import models
+from app import models, config
 from app.users import schema, service, repository
 
 
+settings = config.Settings()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
@@ -61,6 +67,35 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
 
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    """
+    Method to create JWT access token for logged in user
+
+    Parameters:
+    ----------
+        data: Dict
+            a dictionary with email the email as sub
+        expires_delta: timedelta
+            the number of minutes the token will be valid for.
+
+    Returns:
+    -------
+        access_token: The generated access token
+    """
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(
+        claims=to_encode,
+        key=settings.SECRET_KEY,
+        algorithm=settings.ALGORITHM
+    )
+    return encoded_jwt
+
+
 async def register_user(
         db: Session, user: schema.UserCreate) -> models.User:
     """
@@ -90,3 +125,53 @@ async def register_user(
     user.password = hashed_password
 
     return await service.create_user(db=db, user=user)
+
+
+async def login(email: str, plain_password: str, db: Session):
+    """
+    Method to log in a user
+
+    Parameters:
+    ----------
+        email: str
+            the email of user trying to log in
+        plain_password: str
+            plain password to be checked.
+
+    Returns:
+    -------
+        access_token: The generated access token
+
+    Raises
+    ------
+        HTTPException:
+            Authorization error if invalid credentials are supplied.
+    """
+    error = False
+
+    user = await service.get_by_email(db=db, email=email)
+
+    if not user:
+        error = True
+
+    if not error and not verify_password(
+            plain_password=plain_password, hashed_password=user.password):
+        error = True
+
+    if error:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials")
+
+    access_token_expires = timedelta(
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    access_token = create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+
+    return {
+        "token_type": "bearer",
+        "access_token": access_token,
+        "expires_in": access_token_expires.total_seconds(),
+    }

70dabee1be718e4a0949112c27cbaeb783e1c1ee
Gichia <petergichia35@gmail.com>
2022-10-19 14:31:06 +0300

    add test that error is raised if user is not found on user service

--- app/tests/integration/test_address.py
+++ app/tests/integration/test_address.py
@@ -63,3 +63,67 @@ async def test_repository_crud():
     data = await repository.get_address_by_id(db=db, addr_id=1)
 
     assert data is None
+
+
+@pytest.mark.anyio
+async def test_service_crud():
+    test_user = schema.UserCreate(
+        first_name="Test",
+        email="test@test.com",
+        password="password",
+    )
+
+    test_address = schema.AddressCreate(
+        city="Nairobi",
+        contry="Kenya",
+        province="Province",
+        state="",
+        user_id=1,
+        zip=50089,
+    )
+
+    update_address = schema.AddressCreate(
+        city="Kampala",
+        contry="Uganda",
+        province="Province",
+        state="Uganda",
+        user_id=1,
+        zip=79689,
+    )
+
+    await repository.delete_address(db=db, addr_id=1)
+
+    data = await service.create_address(
+        db=db, email=test_user.email, address=test_address)
+
+    # assert data.zip == 50089
+    # assert data.city == "Nairobi"
+    # assert data.contry == "Kenya"
+    # assert data.province == "Province"
+
+    # addr_data = await repository.get_address_by_id(db=db, addr_id=1)
+
+    # assert addr_data.id == 1
+    # assert addr_data.state == ""
+    # assert addr_data.user_id == 1
+    # assert addr_data.zip == 50089
+    # assert addr_data.city == "Nairobi"
+    # assert addr_data.contry == "Kenya"
+    # assert addr_data.province == "Province"
+
+    # address = await repository.update_address(
+    #     db=db, addr_id=1, address=update_address)
+
+    # assert address.id == 1
+    # assert address.user_id == 1
+    # assert address.zip == 79689
+    # assert address.state == "Uganda"
+    # assert address.city == "Kampala"
+    # assert address.contry == "Uganda"
+    # assert address.province == "Province"
+
+    # await repository.delete_address(db=db, addr_id=1)
+
+    # data = await repository.get_address_by_id(db=db, addr_id=1)
+
+    # assert data is None
